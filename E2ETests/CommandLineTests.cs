1146a973fd49aff73b11abd9cbc26eba42d2124e
Andy Gocke <angocke@microsoft.com>
2017-08-03 00:02:26 -0700

    Add a test to validate the error message change

--- E2ETests/CommandLineTests.cs
+++ E2ETests/CommandLineTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,8 +17,8 @@ namespace E2ETests
             using (var xresFile = TempFile.Create(TestResources.SampleXresourcesFile))
             using (var lnkFile = TempFile.Create(TestResources.BlankLnkFile))
             {
-                var exit = await RunXres2Lnk($"{xresFile.FilePath} {lnkFile.FilePath}");
-                Assert.Equal(0, exit);
+                var result = await RunXres2Lnk(xresFile.FilePath, lnkFile.FilePath);
+                Assert.Equal(0, result.ExitCode);
                 var content = File.ReadAllBytes(lnkFile.FilePath);
                 var sha = BitConverter.ToString(SHA256.Create().ComputeHash(content));
                 Assert.Equal("CE6D400FDEFB95455FA91902A4C524543F2ABA1DB85B78FAE2105888686EF8B2",
@@ -24,24 +26,77 @@ namespace E2ETests
             }
         }
 
-        private static Task<int> RunXres2Lnk(string args)
+        [Fact]
+        public async Task GoodXresErrorMessage()
         {
-            s_xresInfo.Arguments = args;
-            var tcs = new TaskCompletionSource<int>();
+            // Random paths that won't exist
+            using (var lnkFile = TempFile.Create(TestResources.SampleXresourcesFile))
+            {
+                var xresPath = @"C:\fsdjknbgdf";
+                var result = await RunXres2Lnk(xresPath, lnkFile.FilePath);
+                Assert.NotEqual(0, result.ExitCode);
+                // Error message should contain xres path
+                Assert.Contains(xresPath, result.ErrOutput);
+            }
+        }
+
+        private static Task<(int ExitCode, string Output, string ErrOutput)>
+            RunXres2Lnk(string xresPath, string lnkPath)
+        {
+            s_xresInfo.Arguments = $"{xresPath} {lnkPath}";
+            var tcs = new TaskCompletionSource<(int, string, string)>();
             var proc = new Process();
-            var task = tcs.Task;
+            var output = new StringBuilder();
+            var errOutput = new StringBuilder();
+
             proc.EnableRaisingEvents = true;
-            proc.Exited += delegate { tcs.TrySetResult(proc.ExitCode); };
+            proc.OutputDataReceived += (s, e) =>
+            {
+                if (e.Data != null)
+                {
+                    output.AppendLine(e.Data);
+                }
+            };
+
+            proc.ErrorDataReceived += (s, e) =>
+            {
+                if (e.Data != null)
+                {
+                    errOutput.AppendLine(e.Data);
+                }
+            };
+
+            proc.Exited += delegate
+            {
+                Task.Run(() =>
+                {
+                    proc.WaitForExit();
+                    var result = (proc.ExitCode,
+                         output.ToString(),
+                         errOutput.ToString());
+                    tcs.TrySetResult(result);
+                });
+            };
+
             proc.StartInfo = s_xresInfo;
             proc.Start();
-            return task;
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+            return tcs.Task;
         }
 
         private static ProcessStartInfo s_xresInfo = new ProcessStartInfo
         {
-            FileName = Path.Combine(
-                Path.GetDirectoryName(typeof(CommandLineTests).Assembly.CodeBase),
-                "xres2lnk.exe")
+            FileName = GetExePath(),
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
         };
+
+        private static string GetExePath()
+        {
+            var asmPath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            return Path.Combine(Path.GetDirectoryName(asmPath), "xres2lnk.exe");
+        }
     }
 }
